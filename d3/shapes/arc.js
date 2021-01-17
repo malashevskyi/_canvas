93914106ca6cf3df29615089e6316454578ba908
malashevskyi <shevsky1488@gmail.com>
2021-01-17 21:49:40 +0200

    d3 shapes arc

---
+++ d3/shapes/arc.js
@@ -0,0 +1,80 @@
+const canvasSketch = require('canvas-sketch');
+import * as d3 from 'd3';
+
+const settings = {
+  // For SVG to resize easily we will have to set this to true
+  scaleToView: true,
+  // Do not append <canvas> element
+  parent: false,
+  dimensions: [512, 512],
+};
+
+const sketch = ({ width, height }) => {
+  d3.select('head').append('style').text(/* css */ `
+    .svg {
+      border: 1px solid #ccc;
+      box-sizing: border-box;
+    }
+  `);
+
+  const svg = d3
+    .select('body')
+    .append('svg')
+    .attr('width', width)
+    .attr('height', height)
+    .classed('svg', true);
+
+  const g = svg
+    .append('g')
+    .attr(
+      'transform',
+      `translate(${width * 0.5}, ${height * 0.5})`
+    );
+
+  const arc = d3.arc()({
+    innerRadius: 90,
+    outerRadius: 100,
+    startAngle: Math.PI,
+    endAngle: Math.PI * 2,
+  });
+
+  g.append('path')
+    .attr('d', arc)
+    .attr('fill', 'black')
+    .attr('stroke', 'black');
+
+  return ({
+    exporting,
+    width,
+    height
+  }) => {
+    svg
+      .attr('width', width)
+      .attr('height', height)
+      .attr('viewBox', `0 0 ${width} ${height}`);
+
+    // If exporting, serialize SVG to Blob
+    if (exporting) {
+      // Clone the SVG element and resize to output dimensions
+      const copy = d3
+        .select(svg.node().cloneNode(true))
+        .attr('width', width)
+        .attr('height', height);
+
+      // Make a blob out of the SVG and return that
+      const data = svgToBlob(copy.node());
+      return { data, extension: '.svg' };
+    }
+  };
+};
+
+canvasSketch(sketch, settings);
+
+function svgToBlob(svg) {
+  const svgAsXML = new window.XMLSerializer().serializeToString(
+    svg
+  );
+  return new window.Blob([svgAsXML], {
+    type: 'image/svg+xml',
+  });
+}
