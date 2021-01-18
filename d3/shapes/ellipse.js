cf2b2342f033e003d1de255c66d47e76a8e4a632
malashevskyi <shevsky1488@gmail.com>
2021-01-18 22:39:35 +0200

    update d3 shapes ellipse

--- d3/shapes/ellipse.js
+++ d3/shapes/ellipse.js
@@ -24,15 +24,28 @@ const sketch = ({ width, height }) => {
     .attr('height', height)
     .classed('svg', true);
 
-  for (let i = 0; i < 10; i++) {
+    const count = 10;
+    const maxEllipseW = 150;
+  
+    const rxScale = d3
+      .scaleLinear()
+      .domain([0, 10])
+      .range([0, maxEllipseW])
+    const yScale = d3
+      .scaleLinear()
+      .domain([count, 0])
+      .range([height - 50, 0])
+
     svg
+      .selectAll('ellipse')
+      .data(Array(10).fill(''))
+      .enter()
       .append('ellipse')
-      .attr('cx', width / 2 - 10 * i)
-      .attr('cy', height - (height - 100) / 10 * i - 50)
-      .attr('rx', 150 - 10 * i)
+      .attr('cx', width / 2 - count)
+      .attr('cy', (d, i) => yScale(count - i))
+      .attr('rx', (d, i) => rxScale(count - i))
       .attr('ry', 30)
-      .attr('fill', `hsl(${10 * i + 20}, 50%, 50%)`);
-  }
+      .attr('fill', (d, i) => `hsl(${10 * i + 20}, 50%, 50%)`);
 
   return ({
     exporting,
